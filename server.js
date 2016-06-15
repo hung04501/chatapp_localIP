eab9e276e15411a2fd21fb4e4f9078edba572811
HungNQ <HungNQ@gmail.com>
2016-06-15 17:04:04 +0700

    add new

---
+++ server.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var mongodb = require('mongodb');
+
+var app = express()
+var fs = require('fs');
+var app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+	
+var port = 5000;
+app.set('port', port);
+
+
+app.get('/', function(resuest, response) {
+  response.sendFile(__dirname+'/index.html');
+});
+
+//socket
+
+io.sockets.on('connection', function(socket) {
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('updatechat',socket.username, data);
+	});
+  
+  /*
+  io.sockets.on('connection', function(socket) {
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('updatechat', socket.username, data);
+	});
+  
+  socket.on('adduser', function(username) {
+		socket.username = username;
+		socket.broadcast.emit('new-user', username);
+	});*/
+	
+ //get ipclient
+ socket.on('ipclient', function(ipclient) {
+		socket.username = getProperty(ipclient);
+	});
+});
+
+//map ip client
+var map = new Object(); // or var map = {};
+//read file
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: fs.createReadStream('listip.txt')
+});
+
+rl.on('line', function (line) {
+  //console.log('Line from file:', line);
+  var ipuser = line.substring(0, line.indexOf("@"));
+  var nickname = line.substring(16, line.length);
+  //set nickname to ip
+  map[ipuser] = nickname;
+  
+  console.log(getProperty(ipuser)+'\n');
+  console.log(map.getKeyByValue(nickname)+'\n');
+});
+
+//get value map theo key
+var getProperty = function get(k) {
+    return map[k];
+}
+//get key from value
+Object.prototype.getKeyByValue = function( value ) {
+    for( var prop in this ) {
+        if( this.hasOwnProperty( prop ) ) {
+             if( this[ prop ] === value )
+                 return prop;
+        }
+    }
+}
+
+
+//port
+server.listen(port, function () {
+  console.log('Listening on port ' + server.address().port)
+});
